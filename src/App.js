ca85931d5de1171320106809bc7bfcb549887019
MichaelOppong <michael@scripted.org>
2020-02-07 19:50:17 +0000

    updated starter code

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ function App() {
     <div className="App">
         <h1> Jeopardy </h1>
         <div className="board">
+            {/* Column - 1 (HISTORY) */}
             <div className="column">
 
             </div>
+            {/* Column - 2 (SPORTS) */}
             <div className="column">
 
             </div>
+            {/* Column - 3 (TECH) */}
             <div className="column">
 
             </div>
