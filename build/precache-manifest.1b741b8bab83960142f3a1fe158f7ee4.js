e87409d2049f7af1d7de44345272500a746ed9bf
MichaelOppong <michael@scripted.org>
2020-02-07 20:52:05 +0000

    updated starter code

---
+++ build/precache-manifest.1b741b8bab83960142f3a1fe158f7ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b0378021648af84d00705fd53304a4",
+    "url": "/jeopardy_sol/index.html"
+  },
+  {
+    "revision": "7877af70c07386c30238",
+    "url": "/jeopardy_sol/static/css/main.c0e0d948.chunk.css"
+  },
+  {
+    "revision": "d1c9af174a90e79a4f7a",
+    "url": "/jeopardy_sol/static/js/2.09d4f401.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jeopardy_sol/static/js/2.09d4f401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7877af70c07386c30238",
+    "url": "/jeopardy_sol/static/js/main.2f1ea8e0.chunk.js"
+  },
+  {
+    "revision": "045349e6b3875970389e",
+    "url": "/jeopardy_sol/static/js/runtime-main.9a9ee7f2.js"
+  }
+]);
